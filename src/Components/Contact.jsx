eef0c347f01538ebfe080d13c6fd2cd26f011722
DonNavinS <seneviratnanavin@gmail.com>
2022-01-21 18:06:23 -0500

    Emails can be sent from contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs, { init, send } from "@emailjs/browser";
+init("user_UC3rhqJHC4MA5dqCjw4m7");
 
 function Contact() {
-  return <div>CONTACT</div>;
+  const form = useRef();
+
+  const sendEmail = () => {
+    emailjs
+      .sendForm(
+        "service_0ic8i6w",
+        "template_u3q8vyj",
+        form.current,
+        "user_UC3rhqJHC4MA5dqCjw4m7"
+      )
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <input type="text" name="from_name" placeholder="Name" />
+      <input type="text" name="from_email" placeholder="Email" />
+      <textarea
+        name="message"
+        cols="30"
+        rows="10"
+        className="border-2 border-black"
+      />
+      <input type="submit" value="Send" className="border-2 border-black" />
+    </form>
+  );
 }
 
 export default Contact;
