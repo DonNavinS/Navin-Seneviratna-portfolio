493ea791cbd16f089892913f638a0e25dfc04986
DonNavinS <seneviratnanavin@gmail.com>
2022-01-26 10:30:55 -0500

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Navin-Seneviratna-portfolio",
+  base: "/Navin-Seneviratna-portfolio/",
   plugins: [react()],
 });
