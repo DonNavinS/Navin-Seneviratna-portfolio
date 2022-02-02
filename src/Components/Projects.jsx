b6b6f47628c12d78339847ddd050216af197bf24
DonNavinS <seneviratnanavin@gmail.com>
2022-02-01 21:05:54 -0500

    updated projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
       <div className="grid grid-cols-12 gap-y-16 ">
         <ProjectDisplay
           title="Restaurant Rolodex"
-          text="React, Express, mySQL"
+          text="REST API, React, Express, mySQL"
           link="https://restaurant-rolodex.herokuapp.com/home"
           github="https://github.com/DonNavinS/Restaurant-Rolodex"
           image="RestRoloImage"
@@ -17,23 +17,24 @@ function Projects() {
           start="col-start-2"
         />
         <ProjectDisplay
+          title="IMDb Actor Game"
+          text="IMDb API, React, Tailwind"
+          link="https://donnavins.github.io/home"
+          github="https://github.com/DonNavinS/imdb-actor-game"
+          image="IMDbGameImage"
+          margin="-translate-x-10"
+          start="col-start-8"
+        />
+        <ProjectDisplay
           title="What's Happening"
           text="Javascript, Express, GoogleAPI"
           link="https://teamtoo222.github.io/Project-1-UofT/"
           github="https://github.com/Teamtoo222/Project-1-UofT"
           image="WhatsHappeningImage"
-          margin="-translate-x-10"
-          start="col-start-8"
-        />
-        <ProjectDisplay
-          title="SmartFix"
-          text="Node, Express, mySQL"
-          link="https://floating-cove-91289.herokuapp.com/"
-          github="https://github.com/groupsixzerotwo/smart-fix"
-          image="SmartFixImage"
           margin="translate-x-24"
           start="col-start-2"
         />
+
         <ProjectDisplay
           title="Employee Profile Generator"
           text="Node, CLI Application"
